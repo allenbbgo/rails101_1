104d93b07396b0f52d3919ca18366b74ecd107e2
allen <>
2018-11-15 11:24:09 +0800

    flash ok

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+    def index 
+        flash[:notice] = "good"
+        flash[:alert]="goodnight"
+        flash[:warning]="fail"
+
+    end
+
+
 end

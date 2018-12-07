aec37ef2507b1b4e9c9a1438a2c1e06143789e8c
allen <>
2018-12-07 18:06:43 +0800

    add post order

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -24,7 +24,7 @@ before_action :find_group_and_check_permission, only: [:edit,:update,:destroy]
 
     def show
         @group = Group.find(params[:id])
-        @post = @group.posts
+        @post = @group.posts.recentt
     end
     def edit
         find_group_and_check_permission
